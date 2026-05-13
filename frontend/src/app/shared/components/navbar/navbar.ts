5ae5a42b9860dc0b64df8b917065d77ddbec6679
Logon2390 <jorgeluis7040@gmail.com>
2026-05-12 19:42:38 -0600

    Agrega componente Navbar con funcionalidad de botón y pruebas unitarias

---
+++ frontend/src/app/shared/components/navbar/navbar.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Button } from '../button/button';
+
+export interface NavItem {
+  label: string;
+  route?: string;
+  active?: boolean;
+}
+
+@Component({
+  selector: 'app-navbar',
+  imports: [Button, CommonModule, FormsModule],
+  templateUrl: './navbar.html',
+})
+export class Navbar {
+  @Input() title: string = 'Post';
+  @Input() buttonText: string = 'Nuevo Post';
+  @Input() buttonIcon: string = 'material-symbols--post-add';
+
+  onCreateClick(): void {
+    console.log('Create clicked');
+  }
+}
