e67129865b4c773d90f9ac84feb4219b0dd63f0a
Logon2390 <jorgeluis7040@gmail.com>
2026-05-13 13:54:39 -0600

    Agrega configuración de rutas y proveedor HttpClient en app.config.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { PostsPage } from '@features/posts/pages/posts/posts';
+import { Details } from '@features/posts/pages/details/details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsPage
+  },
+  {
+    path: 'post/:id',
+    component: Details
+  }
+];
