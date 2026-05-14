464e171ea1fb84d7e22e4543f6a1527dc1208e52
Logon2390 <jorgeluis7040@gmail.com>
2026-05-14 15:38:58 -0600

    Actualiza el template para usar pipe personalizado

--- frontend/src/app/shared/components/user/user.ts
+++ frontend/src/app/shared/components/user/user.ts
@@ -1,16 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { InitialsPipe } from '@app/shared/pipes/initials.pipe';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [InitialsPipe],
   templateUrl: './user.html',
 })
 export class User {
-  @Input() name: string = 'Anonymous';
-  initials(): string {
-    const parts = this.name.trim().split(/\s+/);
-    const first = parts[0]?.[0] ?? 'A';
-    const second = parts[1]?.[0] ?? '';
-    return `${first}${second}`.toUpperCase();
-  }
+   readonly userName: string = localStorage.getItem('name') || 'Anonymous';
 }
