1cb01c12ece609244602b8b63342951eed8f71a9
Logon2390 <jorgeluis7040@gmail.com>
2026-05-14 15:40:51 -0600

    Actualiza componente Navbar para implementacion de authService y renderizado condicional segun isLoggedIn

--- frontend/src/app/shared/components/navbar/navbar.ts
+++ frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,6 +1,5 @@
 import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
 import { AuthService } from '@app/features/home/services/auth.service';
 import { User } from '../user/user';
 import { Button } from '../button/button';
@@ -12,14 +11,12 @@ import { Button } from '../button/button';
 })
 export class Navbar {
   @Input() title = 'Post App';
-
-  private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
 
   readonly userName = this.authService.userName;
   readonly isLoggedIn = this.authService.isLoggedIn;
 
   onLogout(): void {
-    this.authService.logout(this.router);
+    this.authService.logout();
   }
 }
